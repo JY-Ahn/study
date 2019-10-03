fbc22379dfd9e57db853539a5c6089d9297c8f23
Junyong Ahn <junyongsw92@gmail.com>
2019-10-03 13:48:40 +0900

    Update - use hashmap

--- Algorithm/CodeStates/Underbar_memoize.js
+++ Algorithm/CodeStates/Underbar_memoize.js
@@ -1,58 +1,59 @@
 /*
 - Description
-    This function is hand-made version of memoize in Underscore https://underscorejs.org/
+    This function is hand-made version of memoize in Underscore https://underscorejs.org/.
 */
 
 
 _.memoize = function(func) {
     // Even if the function(func) is already been called, should be called again if arguments are different
-    var memo_arguments=[];    // List of arguments already been used
-    var memo = [];            // List of the result
-    var memo_i;               // Use it when choose the already have value
-    var alreadyCalled = false;// functions's status(runned or not)
-    var result = func;        // result
-    var is_same_input = false;// Check if arguments are already used
-    var memo_func = [];       // List of the function already been used
-
+    function ArrayEqual(a, b){
+      if ((a.length === 1) && (b.length === 1) && (a[0] === a[0])){
+        return true;
+      }
+      if (a[0].length !== b[0].length){
+        return false;
+      }
+      for (var i = 0; i < a.length; i++) {
+        if (a[0][i] !== b[i]){
+          return false;
+        }
+      }
+      return true;
+    }
+    
+    var map = new Map();
+    var lookUptable_arg = [];
+    var isSameinput = false;
 
     return function(){
-      if(memo_func.includes(func)){   // Check if function was already used
-        alreadyCalled = true;
+      // func: new function
+      if(!map.has(func)){
+        lookUptable_arg.push([...arguments]);
+        map.set(func, func(...arguments));
+        return map.get(func);
       }else{
-        alreadyCalled = false;
-      }
-      
-      if(!alreadyCalled){                    // New function => calculate the result
-        memo_func.push(func);                // Put func in memo_func
-        alreadyCalled = true;                // Mark it as used
-        result = func(...arguments);         // Calculate the result
-        memo.push(result);                   // Records the result
-        memo_arguments.push([...arguments]); // Records the arguments
-
-      }else{  // Used function => Calculate it or take the value from the memo(Depends on arguments)
-        // Check if arguments were already used
-        for(let i=0 ; i<memo_arguments.length ; i++){
-          if(_.reduce(memo_arguments[i], (acc,cur) => acc+cur, 0)
-          === _.reduce([...arguments], (acc,cur) => acc+cur ,0)){
-            memo_i = i;
-            is_same_input = true;
-          }else{
-            is_same_input = false;
+        for(let i=0 ; i<lookUptable_arg.length ; i++){
+          if(lookUptable_arg[i].length === [...arguments].length){
+            if(ArrayEqual(lookUptable_arg[i], [...arguments])){
+              isSameinput = true;
+            }
           }
         }
+        // func: already called before
+        // arguments: Result of func(arguments) was stored. Just use them
+        if(isSameinput){
 
-        if(!is_same_input){
-          // Function was already used
-          // but arguments are new => Calculate the result
-          result = func(...arguments);            // Calculate the result
-          memo.push(result);                      // Records the result
-          memo_arguments.push([...arguments]);    // Records the arguments
+          isSameinput = false;
+          return map.get(func);
+
         }else{
-          // Function was already used
-          // & arguments were also used bevore => Take the result from memo
-          result = memo[memo_i];
+          // func has been called but arguments are new. 
+          lookUptable_arg.push([...arguments]);
+          map.set(func, func(...arguments));
+          isSameinput = false;
+          return map.get(func); 
         }
+        
       }
-      return result;
     };
   };
